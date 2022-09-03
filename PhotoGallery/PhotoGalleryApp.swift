96ff1517a9cc4a6a034dcb08576f7bb9823fc9ff
Savvin Maksim <40721470+savvinm@users.noreply.github.com>
2022-09-03 14:18:51 +0300

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Maksim Savvin on 03.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
